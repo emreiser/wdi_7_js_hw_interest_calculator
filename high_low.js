bf5e557a44ed120ea3e9c3aecdf810d3eba59e5b
emreiser <emilie.reiser@gmail.com>
2014-02-18 12:05:21 -0500

    Added basic interest calc and guessing number game.

--- high_low.js
+++ high_low.js
@@ -1,1 +1,11 @@
- 
+var number, guess;
+number = Math.floor(Math.random() * 10) + 1;
+guess = parseInt(prompt("Guess a number between 1 and 10"));
+
+if(guess === number) {
+	alert("Correct!");
+} else if (guess > number) {
+	alert("Too high");
+} else {
+	alert("Too low.");
+}
--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,17 @@
- 
+var loan, rate, years, solution;
+
+loan = parseFloat(prompt("How much is your initial loan amount?"));
+years = parseFloat(prompt("How many years is the loan for?"));
+rate = parseFloat(prompt("What was the interest rate? (%)"))/100;
+
+
+solution = loan;
+
+var i = 1
+
+while(i <= years) {
+	solution = solution + (solution * rate);
+	i = i + 1;
+}
+
+console.log(solution)
